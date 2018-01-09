dc267e0c68b63ff1ae15b36d9070c3f341728723
root <root@becker-rom.c.ferrous-tine-191022.internal>
2018-01-09 07:48:08 +0000

    bleh

--- cardinal.mk
+++ cardinal.mk
@@ -22,10 +22,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/gemini/device.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/cardinal/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_gemini
+PRODUCT_NAME := cardinal_gemini
 PRODUCT_DEVICE := gemini
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 5
@@ -41,4 +41,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 TARGET_VENDOR := Xiaomi
 
-JDC_BUILD_TYPE := BETA-EAS
